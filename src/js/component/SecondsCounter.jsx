97feae927d9301e88563a6714f8570e6e8406838
jexuma21 <jerryexuma21@gmail.com>
2022-11-06 04:24:00 +0000

    done counter

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -6,10 +6,10 @@ const SecondsCounter = (props) => {
 	return (
 		<div className="container">
 			<div className="counter">
-				<p>1</p>
-				<p>1</p>
-				<p>1</p>
-				<p>1</p>
+				<p>{props.numSix}</p>
+				<p>{props.numFive}</p>
+				<p>{props.numFour}</p>
+				<p>{props.numThree}</p>
 				<p>{props.numTwo}</p>
 				<p>{props.numOne}</p>
 			</div>
@@ -22,6 +22,10 @@ const SecondsCounter = (props) => {
 SecondsCounter.propTypes = {
 	numOne: PropTypes.number,
 	numTwo: PropTypes.number,
+	numThree: PropTypes.number,
+	numFour: PropTypes.number,
+	numFive: PropTypes.number,
+	numSix: PropTypes.number,
 }
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -13,9 +13,13 @@ let count = 0;
 setInterval (() => {
     const numberOne = Math.floor(count / 1) % 10;
     const numberTwo = Math.floor(count / 10) % 10;
+    const numberThree = Math.floor(count/ 100) % 10;
+    const numberFour = Math.floor(count/ 1000) % 10;
+    const numberFive = Math.floor(count/ 10000) % 10;
+    const numberSix = Math.floor(count/ 100000) % 10;
     count++;
 
-    ReactDOM.render(<SecondsCounter numOne={numberOne} numTwo={numberTwo}/>, document.querySelector("#app"));
+    ReactDOM.render(<SecondsCounter numOne={numberOne} numTwo={numberTwo} numThree={numberThree} numFour={numberFour} numFive={numberFive} numSix={numberSix}/>, document.querySelector("#app"));
 
 }, 1000);
 
